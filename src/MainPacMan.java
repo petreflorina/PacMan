fd2ba20406dd823adce8eafffbbca0e05ca649d5
Petre Florina <petreflorinamaria@gmai.com>
2014-10-29 12:38:26 +0200

    Display fixed

--- src/MainPacMan.java
+++ src/MainPacMan.java
@@ -1,9 +1,31 @@
 
-public class main {
+import static org.lwjgl.opengl.GL11.*;
+import org.lwjgl.opengl.*;
+import org.lwjgl.*;
 
+
+public class MainPacMan {
+
+	public MainPacMan(){
+		try{
+			Display.setDisplayMode(new DisplayMode(640, 480));
+			Display.setTitle("PacMan");
+			Display.create();
+		}
+		catch (LWJGLException e) {
+			e.printStackTrace();
+		}
+		
+		while (!Display.isCloseRequested()) {
+			Display.update();
+			Display.sync(60);
+		}
+		
+		Display.destroy();
+	}
+	
 	public static void main(String[] args) {
-		System.out.print("Hello World!");
-
+		new MainPacMan();
 	}
 
 }
